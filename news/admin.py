925aa1b104fae0f3596fb312f4bdacd53477bc1d
germannp <philipp.germann@embl.es>
2022-04-15 21:44:37 +0200

    Add news copypasta
    Sources:
    https://djangocentral.com/building-a-blog-application-with-django/
    https://www.youtube.com/watch?v=PtQiiknWUcI

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_on")
+    search_fields = ["author", "content"]
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
--- news/views.py
+++ news/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.all().order_by("-created_on")
+    paginate_by = 3
+    template_name = "news/index.html"
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "news/post.html"
