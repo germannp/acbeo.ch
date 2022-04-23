14989cce2316d28676aeba7e3f06ee1f5846592c
germannp <philipp.germann@embl.es>
2022-04-23 17:28:07 +0200

    Use integer choices for signup's status

--- trainings/models.py
+++ trainings/models.py
@@ -15,22 +15,20 @@ class Training(models.Model):
 
 
 class Singup(models.Model):
-    class Status(models.TextChoices):
-        WAIT = "🟡", _("Wait")
-        SELECTED = "🟢", _("Selected")
-        CANCELED = "❌", _("Canceled")
+    Status = models.IntegerChoices("Status", "Selected Waiting Canceled")
 
     pilot = models.ForeignKey(User, on_delete=models.CASCADE, related_name="signups")
     training = models.ForeignKey(
         Training, on_delete=models.CASCADE, related_name="signups"
     )
-    status = models.CharField(max_length=1, choices=Status.choices, default=Status.WAIT)
+    status = models.IntegerField(choices=Status.choices, default=Status.Waiting)
     comment = models.CharField(max_length=150, default="", blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = (("pilot", "training"),)
+        ordering = ["status", "created_on"]
 
     def __str__(self):
         return f"{self.pilot} for {self.training}"
