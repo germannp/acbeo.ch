b8f67615bc28211f172b5a271cf8e4e42a0f3337
germannp <philipp.germann@embl.es>
2022-05-04 22:52:35 +0200

    Sanitize forwarding when updating signup

--- trainings/views.py
+++ trainings/views.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, resolve
 from django.views import generic
 
 from .models import Training, Signup
@@ -126,7 +126,7 @@ class SignupUpdateView(LoginRequiredMixin, generic.UpdateView):
         return signup
 
     def get_success_url(self):
-        next = self.request.GET.get("next", None)
-        if next:
+        next = self.request.GET.get("next")
+        if next == reverse_lazy("my_signups"):
             return next
         return reverse_lazy("trainings")
