809423111e72168cd0ce6bf606ebd7e17023d1df
germannp <philipp.germann@embl.es>
2023-03-19 15:12:04 +0100

    Add `Pilot.prepaid_flights` to admin panel

--- news/admin.py
+++ news/admin.py
@@ -31,6 +31,7 @@ class PilotAdmin(BaseUserAdmin):
         "last_name",
         "phone",
         "date_joined",
+        "prepaid_flights",
         "role",
         "is_active",
     )
@@ -50,6 +51,7 @@ class PilotAdmin(BaseUserAdmin):
                     "last_name",
                     "email",
                     "phone",
+                    "prepaid_flights",
                     "role",
                     "is_active",
                     "password1",
@@ -69,6 +71,7 @@ class PilotAdmin(BaseUserAdmin):
                     "last_name",
                     "email",
                     "phone",
+                    "prepaid_flights",
                     "role",
                     "is_active",
                     "password",
